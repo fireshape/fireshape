1ff2cd1602b37b30218ff291ef97b6c602308211
Florian Wechsung <f.wechsung@gmail.com>
2019-04-26 15:29:37 +0100

    more cleanup

--- examples/L2tracking/L2tracking_PDEconstraint.py
+++ examples/L2tracking/L2tracking_PDEconstraint.py
@@ -7,23 +7,19 @@ class PoissonSolver(PdeConstraint):
     """A Poisson BVP with hom DirBC as PDE constraint."""
     def __init__(self, mesh_m):
         super().__init__()
-        self.mesh_m = mesh_m
 
         # Setup problem
-        self.V = fd.FunctionSpace(self.mesh_m, "CG", 1)
-
-        # Preallocate solution variables for state and adjoint equations
-        self.solution = fda.Function(self.V, name="State")
+        V = fd.FunctionSpace(mesh_m, "CG", 1)
 
         # Weak form of Poisson problem
-        u = self.solution
-        v = fd.TestFunction(self.V)
-        self.f = fda.Constant(4.)
-        self.F = (fd.inner(fd.grad(u), fd.grad(v)) - self.f * v) * fd.dx
-        self.bcs = fda.DirichletBC(self.V, 0., "on_boundary")
+        u = fda.Function(V, name="State")
+        v = fd.TestFunction(V)
+        f = fda.Constant(4.)
+        F = (fd.inner(fd.grad(u), fd.grad(v)) - f * v) * fd.dx
+        bcs = fda.DirichletBC(V, 0., "on_boundary")
 
         # PDE-solver parameters
-        self.params = {
+        params = {
             "ksp_type": "cg",
             "mat_type": "aij",
             "pc_type": "hypre",
@@ -33,10 +29,11 @@ class PoissonSolver(PdeConstraint):
             "ksp_stol": 1e-15,
         }
 
-        stateproblem = fda.NonlinearVariationalProblem(
-            self.F, self.solution, bcs=self.bcs)
+        self.solution = u
+        problem = fda.NonlinearVariationalProblem(
+            F, self.solution, bcs=bcs)
         self.solver = fda.NonlinearVariationalSolver(
-            stateproblem, solver_parameters=self.params)
+            problem, solver_parameters=params)
 
     def solve(self):
         super().solve()
