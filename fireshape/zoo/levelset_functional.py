c4ed51a57a4817af646d044f9610fa3a6f540c2c
Florian Wechsung <f.wechsung@gmail.com>
2019-07-29 16:02:19 +0100

    add ability to pass a measure to the LevelsetFunctional
    One might for example want to pass ds(domain=mesh) in order to integrate over the boundary, e.g. for a perimeter regularisation

--- fireshape/zoo/levelset_functional.py
+++ fireshape/zoo/levelset_functional.py
@@ -11,12 +11,16 @@ class LevelsetFunctional(ShapeObjective):
 
     Optima are zero-levels of ufl function f.
     """
-    def __init__(self, f, *args, **kwargs):
+    def __init__(self, f, *args, measure=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.f = f
+        if measure is None:
+            self.measure = fd.dx(domain=self.Q.mesh_m, degree=30)
+        else:
+            self.measure = measure
 
     def value_form(self):
-        return self.f * fd.dx(domain=self.Q.mesh_m)
+        return self.f * self.measure
 
     def derivative_form(self, v):
         X = fd.SpatialCoordinate(self.Q.mesh_m)
