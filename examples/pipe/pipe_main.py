a761c740d9193d7011c75c148697bfa69111fd27
aldapa <paganini@maths.oc.ac.uk>
2019-08-28 11:18:18 +0100

    copy files from ap/pipe

---
+++ examples/pipe/pipe_main.py
@@ -0,0 +1,81 @@
+import firedrake as fd
+import fireshape as fs
+import fireshape.zoo as fsz
+import ROL
+from pipe_PDEconstraint import NavierStokesSolver
+from pipe_objective import PipeObjective
+
+# setup problem
+mesh = fd.Mesh("pipe.msh")
+Q = fs.FeControlSpace(mesh)
+inner = fs.LaplaceInnerProduct(Q, fixed_bids=[1, 2, 3])
+q = fs.ControlVector(Q, inner)
+
+# setup PDE constraint
+dim = mesh.topological_dimension()
+if dim == 2:
+    viscosity = fd.Constant(1./400.)
+elif dim ==3:
+    viscosity = fd.Constant(1/10.)
+else:
+    raise NotImplementedError
+e = NavierStokesSolver(Q.mesh_m, viscosity)
+
+# save state variable evolution in file u.pvd
+e.solve()
+out = fd.File("u.pvd")
+def cb(): return out.write(e.solution.split()[0])
+cb()
+
+# create PDEconstrained objective functional
+J_ = PipeObjective(e, Q, cb=cb)
+J = fs.ReducedObjective(J_, e)
+
+Jq = fsz.MoYoSpectralConstraint(10, fd.Constant(0.5), Q)
+J = J + Jq
+
+# volume constraint
+class VolumeFunctional(fs.ShapeObjective):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # physical mesh
+        self.mesh_m = self.Q.mesh_m
+
+    def value_form(self):
+        # volume integral
+        return fd.Constant(1.0) * fd.dx(domain=self.mesh_m)
+
+
+vol = VolumeFunctional(Q)
+initial_vol = vol.value(q, None)
+econ = fs.EqualityConstraint([vol], target_value=[initial_vol])
+emul = ROL.StdVector(1)
+
+# ROL parameters
+params_dict = {
+    'General': {
+        'Secant': {'Type': 'Limited-Memory BFGS',
+                   'Maximum Storage': 5}},
+    'Step': {
+        'Type': 'Augmented Lagrangian',
+        'Line Search': {'Descent Method': {
+            'Type': 'Quasi-Newton Step'}
+        },
+        'Augmented Lagrangian': {
+            'Subproblem Step Type': 'Line Search',
+            'Penalty Parameter Growth Factor': 1.04,
+            'Print Intermediate Optimization History': True,
+            'Subproblem Iteration Limit': 5
+        }},
+    'Status Test': {
+        'Gradient Tolerance': 1e-2,
+        'Step Tolerance': 1e-1,
+        'Constraint Tolerance': 1e-3,
+        'Iteration Limit': 20}
+}
+params = ROL.ParameterList(params_dict, "Parameters")
+problem = ROL.OptimizationProblem(J, q, econ=econ, emul=emul)
+solver = ROL.OptimizationSolver(problem, params)
+solver.solve()
+print(vol.value(q, None) - initial_vol)
---
+++ examples/pipe/pipe_objective.py
@@ -0,0 +1,18 @@
+import firedrake as fd
+from fireshape import ShapeObjective
+from pipe_PDEconstraint import NavierStokesSolver
+
+
+class PipeObjective(ShapeObjective):
+    """L2 tracking functional for Poisson problem."""
+
+    def __init__(self, pde_solver: NavierStokesSolver, *args,  **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pde_solver = pde_solver
+
+    def value_form(self):
+        """Evaluate misfit functional."""
+        nu = self.pde_solver.viscosity
+        z = self.pde_solver.solution
+        u, p = fd.split(z)
+        return nu * fd.inner(fd.grad(u), fd.grad(u)) * fd.dx
