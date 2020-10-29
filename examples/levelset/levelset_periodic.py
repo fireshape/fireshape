af638267194e91a069cd523e4f0bb0f7b3739691
APaganini <aldapa@gmail.com>
2020-10-29 22:46:22 +0000

    added periodic test

--- examples/levelset/levelset_periodic.py
+++ examples/levelset/levelset_periodic.py
@@ -3,20 +3,34 @@ from fireshape import *
 import numpy as np
 import ROL
 
+d = 3
+
 # goal: update the mesh coordinates of a periodic mesh
 # so that a function defined on the mesh becomes a given one
-mesh = PeriodicSquareMesh(30, 30, 1)
+if d == 2:
+    mesh = PeriodicUnitSquareMesh(30, 30)
+elif d== 3:
+    mesh = PeriodicUnitCubeMesh(20, 20, 20)
 Q = PeriodicControlSpace(mesh)  #how can we fix the boundary?
-inner = LaplaceInnerProduct(Q)
+#inner = LaplaceInnerProduct(Q)
+inner = H1InnerProduct(Q)
 q = ControlVector(Q, inner)
 
 # save shape evolution in file domain.pvd
 V = FunctionSpace(Q.mesh_m, "DG", 0)
 sigma = Function(V)
-x, y = SpatialCoordinate(Q.mesh_m)
-perturbation = 0.1*sin(x*np.pi)*sin(y*np.pi)**2
-sigma.interpolate(sin(y*np.pi)*(cos(2*np.pi*x*(1+perturbation))))
-f = cos(2*np.pi*x)*sin(y*np.pi)
+if d == 2:
+    x, y = SpatialCoordinate(Q.mesh_m)
+    g = sin(y*np.pi)  # truncate at bdry
+    f = cos(2*np.pi*x)*g
+    perturbation = 0.05*sin(x*np.pi)*g**2
+    sigma.interpolate(g*cos(2*np.pi*x*(1+perturbation)))
+elif d == 3:
+    x, y, z = SpatialCoordinate(Q.mesh_m)
+    g = sin(y*np.pi)*sin(z*np.pi)  # truncate at bdry
+    f = cos(2*np.pi*x)*g
+    perturbation = 0.05*sin(x*np.pi)*g**2
+    sigma.interpolate(g*cos(2*np.pi*x*(1+perturbation)))
 
 class LevelsetFct(ShapeObjective):
     def __init__(self, sigma, f, *args, **kwargs):
@@ -44,7 +58,7 @@ params_dict = {'Step': {'Type': 'Trust Region'},
                        'Maximum Storage': 25}},
 'Status Test': {'Gradient Tolerance': 1e-4,
                 'Step Tolerance': 1e-4,
-                'Iteration Limit': 30}}
+                'Iteration Limit': 60}}
 
 params = ROL.ParameterList(params_dict, "Parameters")
 problem = ROL.OptimizationProblem(J, q)
