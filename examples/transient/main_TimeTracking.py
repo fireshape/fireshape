f32a208904e8e57a7caf1711245286f6176f0b42
APaganini <aldapa@gmail.com>
2020-11-17 18:58:48 +0000

    flaked example

--- examples/transient/main_TimeTracking.py
+++ examples/transient/main_TimeTracking.py
@@ -2,6 +2,7 @@ from firedrake import *
 from fireshape import *
 import ROL
 
+
 class TimeTracking(PDEconstrainedObjective):
     """
     L1-L2 misfit functional for time-dependent problem constrained
@@ -21,8 +22,8 @@ class TimeTracking(PDEconstrainedObjective):
         # target solution, which also specifies rhs and DirBC
         mesh_m = self.Q.mesh_m
         x, y = SpatialCoordinate(mesh_m)
-        self.u_t= lambda t:  sin(pi*x)*sin(pi*y)*cos(t)
-        f = lambda t: sin(pi*x)*sin(pi*y)*(2*pi**2*cos(t) - sin(t))
+        self.u_t = lambda t: sin(pi*x)*sin(pi*y)*cos(t)
+        self.f = lambda t: sin(pi*x)*sin(pi*y)*(2*pi**2*cos(t) - sin(t))
 
         # perturbed initial guess to be fixed by shape optimization
         V = FunctionSpace(mesh_m, "CG", 1)
@@ -32,7 +33,7 @@ class TimeTracking(PDEconstrainedObjective):
 
         # define self.cb, which is always called after self.solvePDE
         self.File = File("u0.pvd")
-        self.cb = lambda : self.File.write(self.u0)
+        self.cb = lambda: self.File.write(self.u0)
 
         # heat equation discretized with implicit Euler
         self.u = Function(V)
@@ -41,13 +42,14 @@ class TimeTracking(PDEconstrainedObjective):
         self.dx = dx(metadata={"quadrature_degree": 1})
         self.dt = 0.125
         v = TestFunction(V)
-        self.F = lambda t, u, u_old: inner((u-u_old)/self.dt,v)*self.dx \
-                                     + inner(grad(u), grad(v))*self.dx \
-                                     - inner(f(t+self.dt), v)*self.dx
+        self.F = lambda t, u, u_old: inner((u-u_old)/self.dt, v)*self.dx \
+            + inner(grad(u), grad(v))*self.dx \
+            - inner(self.f(t+self.dt), v)*self.dx
 
     def solvePDE(self):
         """Solve the heat equation and evaluate the objective function."""
-        self.J = 0; t = 0
+        self.J = 0
+        t = 0
         self.u.assign(self.u0)
         self.J += assemble(self.dt*(self.u - self.u_t(t))**2*self.dx)
 
