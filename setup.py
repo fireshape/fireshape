6a32398f6296a3baa50263459f2717f286b11ed7
APaganini <aldapa@gmail.com>
2023-11-03 14:13:54 +0000

    amend setup.py to avoid failing ROL install on mac

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@ from setuptools import setup
 try:
     import firedrake # noqa
     import firedrake.adjoint # noqa
+    import ROL
 except ImportError:
-    raise Exception("Firedrake needs to be installed and activated. "
-                    "Please visit firedrakeproject.org")
+    raise Exception("Firedrake needs to be installed and activated."
+                    "Please visit firedrakeproject.org ."
+                    "ROL must have been installed, too")
 setup(
     name='fireshape',
     version='0.0.1',
     author='Alberto Paganini, Florian Wechsung',
-    author_email='wechsung@maths.ox.ac.uk',
+    author_email='admp1@leicester.ac.uk',
     description='A library for shape optimization based on firedrake',
     long_description='',
     packages=['fireshape', 'fireshape.zoo'],
     zip_safe=False,
-    install_requires=["roltrilinos", "rol", "scipy"]
+    install_requires=["scipy"] # avoid reinstalling ROL (and fail) on mac
+    #install_requires=["roltrilinos", "rol", "scipy"]
 )
