962d62ea9be6c89e704fc0409bc1d1c6bce21f9d
APaganini <aldapa@gmail.com>
2023-11-18 19:35:29 +0000

    add fedecoupled example

--- examples/levelset/levelset_fedecoupled.py
+++ examples/levelset/levelset_fedecoupled.py
@@ -3,20 +3,19 @@ import fireshape as fs
 import fireshape.zoo as fsz
 import ROL
 
+# create initial domain
 mesh = fd.UnitDiskMesh(refinement_level=2)
-#mesh_c = fd.UnitSquareMesh(5,5)
+
+# create decouple FE-controlspace on mesh_c
 mesh_c = fd.RectangleMesh(10, 10, 1.5, 1.5, -.5, -1.5)
 fd.File("domain_control.pvd").write(mesh_c.coordinates)
-
-# create decouple FE-controlspace
 Q = fs.FeControlSpace(mesh, add_to_degree_r=1, mesh_c=mesh_c, degree_c=2)
 inner = fs.H1InnerProduct(Q, fixed_bids=[1, 2, 3, 4])
 
-# define objective
+# define objective so that optimal shape is with center (0,0) and radius 1.2
 mesh_m = Q.mesh_m
 (x, y) = fd.SpatialCoordinate(mesh_m)
-#f = (pow(x-0.5, 2))+pow(y-0.5, 2) - 2.
-f = (pow(x-0., 2))+pow(y-0., 2) - 1.2**2
+f = (pow(x, 2))+pow(y, 2) - 1.2**2
 out = fd.File("domain.pvd")
 J = fsz.LevelsetFunctional(f, Q, cb=lambda: out.write(mesh_m.coordinates))
 
