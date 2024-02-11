8c46bc3d8baef00cc8fbd15840bd0354378d6ec9
Prem Jagadeesh <prem.jagadeesh1@gmail.com>
2024-02-11 08:35:45 +0000

    add indicator functions

--- examples/levelset_cm/levelset_cm.py
+++ examples/levelset_cm/levelset_cm.py
@@ -2,7 +2,7 @@ import firedrake as fd
 import fireshape as fs
 import ROL
 from levelsetfunctional import LevelsetFunctional
-
+import watchpoints
 # setup problem
 mesh = fd.Mesh("/home/prem/src/github/fireshape/examples/levelset_cm/square_in_square.msh")
 
@@ -10,22 +10,26 @@ S = fd.FunctionSpace(mesh, "DG", 0)
 I = fd.Function(S, name="indicator")
 x = fd.SpatialCoordinate(mesh)
 
-I.interpolate(fd.conditional(x[0] < 1, 
-              fd.conditional(x[0] > 0,
-              fd.conditional(x[1] > 0,
-              fd.conditional(x[1] < 1, 1, 0), 0), 0), 0))
+I.interpolate(fd.conditional(x[0] < 1, fd.conditional(x[0] > 0, fd.conditional(x[1] > 0, fd.conditional(x[1] < 1, 1, 0), 0), 0), 0))
 
 Q = fs.CmControlSpace(mesh, I)
 # inner = fs.LaplaceInnerProduct(Q)
 inner = fs.H1InnerProduct(Q)
 q = fs.ControlVector(Q, inner)
-
+# watchpoints.watch(q.fun)
 
 # save shape evolution in file domain.pvd
-out = fd.File("domain.pvd")
+out = fd.File("100step.pvd")
+
+# TODO: Remove below indicator and fix how to plot in general
+# Just creating this so I can actually plot it onto the output to see how shape changes
+S_2 = fd.FunctionSpace(Q.mesh_m, "DG", 0)
+I_2 = fd.Function(S_2, name="indicator")
+x_m, y_m = fd.SpatialCoordinate(Q.mesh_m)
+I_2.interpolate(fd.conditional(x_m < 1, fd.conditional(x_m > 0, fd.conditional(y_m > 0, fd.conditional(y_m < 1, 1, 0), 0), 0), 0))
 
 # create objective functional
-J = LevelsetFunctional(Q, cb=lambda: out.write(Q.mesh_m.coordinates))
+J = LevelsetFunctional(Q, cb=lambda: out.write(Q.mesh_m.coordinates, I_2))
 
 # ROL parameters
 params_dict = {
@@ -46,7 +50,7 @@ params_dict = {
     'Status Test': {
         'Gradient Tolerance': 1e-4,
         'Step Tolerance': 1e-10,
-        'Iteration Limit': 30
+        'Iteration Limit': 30,
     }
 }
 
--- examples/levelset_cm/levelsetfunctional.py
+++ examples/levelset_cm/levelsetfunctional.py
@@ -1,6 +1,7 @@
 import firedrake as fd
 import fireshape as fs
-
+from icecream import ic
+ic.configureOutput(includeContext=True) 
 
 class LevelsetFunctional(fs.ShapeObjective):
     def __init__(self, *args, **kwargs):
@@ -8,13 +9,19 @@ class LevelsetFunctional(fs.ShapeObjective):
 
         # physical mesh
         self.mesh_m = self.Q.mesh_m
-        self.I = self.Q.I
 
+        S = fd.FunctionSpace(self.mesh_m, "DG", 0)
+        self.I = fd.Function(S, name="indicator")
         # global function defined in terms of physical coordinates
         x, y = fd.SpatialCoordinate(self.mesh_m)
+
+        self.I.interpolate(fd.conditional(x < 1, fd.conditional(x > 0, fd.conditional(y > 0, fd.conditional(y < 1, 1, 0), 0), 0), 0))
         self.f = (x - 0.5)**2 + (y - 0.5)**2 - 0.5
+        # self.f = (x + self.Q.dphi[0] - 0.5)**2 + (y + self.Q.dphi[1] - 0.5)**2 - 0.5
 
     def value_form(self):
         # volume integral
         # TODO: maybe this needs *I idk?
-        return self.f * fd.dx
+        k = self.f * self.I * fd.dx
+        # ic(fd.assemble(k))
+        return k
