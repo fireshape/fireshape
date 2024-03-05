b3f34d4da7ee42b6c4ae5962f14cc6964b49cddc
pj621 <pj621@ic.ac.uk>
2024-03-05 22:34:18 +0000

    setup stoked problem and .geo files for stoke problem in new control space

---
+++ examples/stokes_cm/stoke_control.geo
@@ -0,0 +1,56 @@
+// Gmsh project created on Tue Aug  2 17:26:31 2016
+// inner Rectangle corners 
+Point(1) = {-6, 2, 0, 0.5};
+Point(2) = {-6, -2, 0, 0.5};
+Point(3) = {6, -2, 0, 0.5};
+Point(4) = {6, 2, 0, 0.5};
+
+Line(1) = {1, 4};
+Line(2) = {4, 3};
+Line(3) = {3, 2};
+Line(4) = {2, 1};
+
+// Circle points
+Point(5) = {0, 0, 0, 0.1};
+Point(6) = {0.5, 0, 0, 0.1};
+Point(7) = {-0.5, 0, 0, 0.1};
+Point(8) = {0, 0.5, 0, 0.1};
+Point(9) = {0, -0.5, 0, 0.1};
+
+Circle(5) = {8, 5, 6};
+Circle(6) = {6, 5, 9};
+Circle(7) = {9, 5, 7};
+Circle(8) = {7, 5, 8};
+
+// Exterior mesh points for control
+Point(10) = {-12, 12, 0, 0.5};
+Point(11) = {12, 12, 0, 0.5};
+Point(12) = {12, -12, 0, 0.5};
+Point(13) = {-12, -12, 0, 0.5};
+
+Line(13) = {10, 11};
+Line(14) = {11, 12};
+Line(15) = {12, 13};
+Line(16) = {13, 10};
+
+
+// Two line loops outer rectangle, inner circle
+Line Loop(9) = {1, 2, 3, 4};
+Line Loop(10) = {8, 5, 6, 7};
+Line Loop(17) = {13, 14, 15, 16};
+
+
+Plane Surface(11) = {9, 10};
+Plane Surface(12) = {10};
+Plane Surface(18) = {17, 9};
+
+
+Physical Line("NoSlip") = {1, 3};
+Physical Line("Inflow") = {4};
+Physical Line("Outflow") = {2};
+Physical Line("BallSurface") = {8, 7, 6, 5};
+Physical Line("Boundary") = {13, 14, 15, 16};
+
+Physical Surface("Punched Dim") = {18};
+Physical Surface("Channel") = {11};
+Physical Surface("Ball") = {12};
---
+++ examples/stokes_cm/stokes_cm.py
@@ -0,0 +1,85 @@
+import firedrake as fd
+import fireshape as fs
+import fireshape.zoo as fsz
+
+import ROL
+
+mesh_c = fd.Mesh("stoke_control.msh")
+mesh_r = fd.Mesh("stoke.msh")
+
+# Q = fs.FeControlSpace(mesh)
+# Q = fs.FeMultiGridControlSpace(mesh, refinements=1, degree=1)
+
+S = fd.FunctionSpace(mesh_c, "DG", 0)
+I = fd.Function(S, name="indicator")
+x = fd.SpatialCoordinate(mesh_c)
+
+I.interpolate(fd.conditional(x[0]**2 + x[1]**2 < 0.5**2, 1, 0))
+
+Q = fs.CmControlSpace(mesh_c, mesh_r, I)
+# this 1,2,3 represents the physical curve tag 
+# have added 5 here as this is the complete outside bit
+inner = fs.L2InnerProduct(Q, fixed_bids=[1, 2, 3])
+mesh_m = Q.mesh_m
+
+(x, y) = fd.SpatialCoordinate(mesh_m)
+inflow_expr = fd.Constant((1.0, 0.0))
+e = fsz.StokesSolver(mesh_m, inflow_bids=[1, 2],
+                     inflow_expr=inflow_expr, noslip_bids=[4], direct=False)
+e.solve()
+out = fd.File("u.pvd")
+
+
+def cb(*args):
+    out.write(e.solution.split()[0])
+
+
+cb()
+
+Je = fsz.EnergyObjective(e, Q, cb=cb)
+Jr = 1e-2 * fs.ReducedObjective(Je, e)
+Js = fsz.MoYoSpectralConstraint(10., fd.Constant(0.7), Q)
+Jd = 1e-3 * fsz.DeformationRegularization(Q, l2_reg=1e-2, sym_grad_reg=1e0,
+                                          skew_grad_reg=1e-2)
+J = Jr + Jd + Js
+q = fs.ControlVector(Q, inner)
+g = q.clone()
+
+vol = fsz.LevelsetFunctional(fd.Constant(1.0), Q)
+baryx = fsz.LevelsetFunctional(x, Q)
+baryy = fsz.LevelsetFunctional(y, Q)
+econ = fs.EqualityConstraint([vol, baryx, baryy])
+emul = ROL.StdVector(3)
+
+params_dict = {
+    'General': {
+        'Secant': {
+            'Type': 'Limited-Memory BFGS', 'Maximum Storage': 5
+        }
+    },
+    'Step': {
+        'Type': 'Augmented Lagrangian',
+        'Line Search': {
+            'Descent Method': {
+                'Type': 'Quasi-Newton Step'
+            }
+        },
+        'Augmented Lagrangian': {
+            'Subproblem Step Type': 'Line Search',
+            'Penalty Parameter Growth Factor': 2.,
+            'Print Intermediate Optimization History': True,
+            'Subproblem Iteration Limit': 20
+        }
+    },
+    'Status Test': {
+        'Gradient Tolerance': 1e-4,
+        'Step Tolerance': 1e-5,
+        'Iteration Limit': 4
+    }
+}
+params = ROL.ParameterList(params_dict, "Parameters")
+problem = ROL.OptimizationProblem(J, q, econ=econ, emul=emul)
+solver = ROL.OptimizationSolver(problem, params)
+vol_before = vol.value(q, None)
+solver.solve()
+print(vol.value(q, None)-vol_before)
