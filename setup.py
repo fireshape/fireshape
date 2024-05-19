f4b8c11294a327434f114811cc6e7705053f9d06
Alberto Paganini <20994366+APaganini@users.noreply.github.com>
2024-05-19 20:50:28 +0100

    remove ROL from setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from setuptools import setup
 try:
     import firedrake # noqa
     import firedrake.adjoint # noqa
-    import ROL # noqa
 except ImportError:
     raise Exception("Firedrake needs to be installed and activated."
-                    "Please visit firedrakeproject.org ."
-                    "ROL must have been installed, too")
+                    "Please visit firedrakeproject.org .")
 setup(
     name='fireshape',
-    version='0.0.1',
+    version='1.0.1',
     author='Alberto Paganini, Florian Wechsung',
     author_email='admp1@leicester.ac.uk',
     description='A library for shape optimization based on firedrake',
@@ -17,6 +15,4 @@ setup(
     packages=['fireshape', 'fireshape.zoo'],
     zip_safe=False,
     install_requires=["scipy"]
-    # avoid reinstalling ROL (and fail) on mac
-    # install_requires=["roltrilinos", "rol", "scipy"]
 )
