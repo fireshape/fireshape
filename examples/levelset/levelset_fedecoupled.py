60d35bd0d92444f35928591e95f07dafe2588459
Alberto Paganini <20994366+APaganini@users.noreply.github.com>
2024-05-27 10:44:46 +0100

    updated levelset examples to decoupled meshes

--- examples/levelset/levelset_fedecoupled.py
+++ examples/levelset/levelset_fedecoupled.py
@@ -1,38 +1,32 @@
 import firedrake as fd
 import fireshape as fs
 import fireshape.zoo as fsz
-import ROL
+import petsc4py.PETSc as PETSc
 
 # create initial domain
 mesh = fd.UnitDiskMesh(refinement_level=2)
 
-# create decouple FE-controlspace on mesh_c
+# create decoupled FE-controlspace on mesh_c using CG2 FE
 mesh_c = fd.RectangleMesh(10, 10, 1.5, 1.5, -.5, -1.5)
-fd.File("domain_control.pvd").write(mesh_c.coordinates)
+fd.VTKFile("domain_control.pvd").write(mesh_c.coordinates)
+# use also CG2 FE on mesh_r to represent self.T
 Q = fs.FeControlSpace(mesh, add_to_degree_r=1, mesh_c=mesh_c, degree_c=2)
-inner = fs.H1InnerProduct(Q, fixed_bids=[1, 2, 3, 4])
+Q.assign_inner_product(fs.H1InnerProduct(Q, fixed_bids=[1, 2, 3, 4]))
 
-# define objective so that optimal shape is with center (0,0) and radius 1.2
-mesh_m = Q.mesh_m
-(x, y) = fd.SpatialCoordinate(mesh_m)
+# define objective so that optimal shape is
+# a disc with center (0,0) and radius 1.2
+x, y = fd.SpatialCoordinate(Q.mesh_m)
 f = (pow(x, 2))+pow(y, 2) - 1.2**2
-out = fd.File("domain.pvd")
-J = fsz.LevelsetFunctional(f, Q, cb=lambda: out.write(mesh_m.coordinates))
+J = fsz.LevelsetFunctional(Q, f, usecb=True)
 
-# optimize
-q = fs.ControlVector(Q, inner)
-params_dict = {
-    'General': {
-        'Secant': {'Type': 'Limited-Memory BFGS', 'Maximum Storage': 5}},
-    'Step': {
-        'Type': 'Line Search',
-        'Line Search': {'Descent Method': {'Type': 'Quasi-Newton Step'}}},
-    'Status Test': {
-        'Gradient Tolerance': 1e-1,
-        'Step Tolerance': 1e-2,
-        'Iteration Limit': 10}}
-
-params = ROL.ParameterList(params_dict, "Parameters")
-problem = ROL.OptimizationProblem(J, q)
-solver = ROL.OptimizationSolver(problem, params)
+# PETSc.TAO solver using the limited-memory
+# variable-metric method. Call using
+# python levelset_fedecoupled.py -tao_monitor
+# to print updates in the terminal
+solver = PETSc.TAO().create()
+solver.setType("lmvm")
+solver.setFromOptions()
+solver.setSolution(Q.get_PETSc_zero_vec())
+solver.setObjectiveGradient(J.objectiveGradient, None)
+solver.setTolerances(gatol=1.0e-2, grtol=1.0e-2)
 solver.solve()
--- fireshape/zoo/levelset_functional.py
+++ fireshape/zoo/levelset_functional.py
@@ -7,20 +7,30 @@ __all__ = ["LevelsetFunctional", "VolumeFunctional"]
 class LevelsetFunctional(ShapeObjective):
     """
     Implementation of level-set shape functional.
-
     Optima are zero-levels of ufl function f.
     """
     def __init__(self, Q, f, usecb=False, *args, **kwargs):
         super().__init__(Q, *args, **kwargs)
         self.f = f
-        Vdet = fd.FunctionSpace(self.Q.mesh_r, "DG", 0)
+        if getattr(self, "is_decoupled", False):
+            # if the control is on a different mesh mesh_c,
+            # then compute the determinant of DT there
+            Vdet = fd.FunctionSpace(self.Q.V_c.mesh(), "DG", 0)
+            self.T_c = self.Q.V_c.mesh().coordinates + self.Q.fun
+        else:
+            # otherwise compute detDT on mesh_r
+            Vdet = fd.FunctionSpace(self.Q.mesh_r, "DG", 0)
         self.detDT = fd.Function(Vdet)
-        if usecb and cb is None:  # if True, store meshes in soln.pvd
+        if usecb and self.cb is None:  # if True, store meshes in soln.pvd
             out = fd.VTKFile("soln.pvd")
             self.cb = lambda: out.write(Q.mesh_m.coordinates)
 
     def value_form(self):
-        self.detDT.interpolate(fd.det(fd.grad(self.Q.T)))
+        if getattr(self, "is_decoupled", False):
+            self.detDT.interpolate(fd.det(fd.grad(self.T_c)))
+        else:
+            self.detDT.interpolate(fd.det(fd.grad(self.Q.T)))
+        # approximately check that detDT is not too small
         mesh_is_fine = min(self.detDT.vector()) > 0.01
         if mesh_is_fine:
             return self.f * fd.dx(domain=self.Q.mesh_m)
