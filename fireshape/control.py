638c16e44879e4c434cdd1453b9f7fbd7ba408e9
Alberto Paganini <20994366+APaganini@users.noreply.github.com>
2026-05-06 10:36:12 +0100

    API interpolator

--- fireshape/control.py
+++ fireshape/control.py
@@ -175,15 +175,18 @@ class FeControlSpace(ControlSpace):
             self.V_c_dual = self.V_c.dual()
             # Create interpolator and cointerpolator from V_c into V_r
             self.Ip_v = fd.Function(self.V_c)
+
             interp = fd.interpolate(self.Ip_v, self.V_r,
-                                    default_missing_val=0.)
-            self.Ip = fd.Interpolator(interp, self.V_r,
-                                      allow_missing_dofs=True)
+                                    default_missing_val=0.,
+                                    allow_missing_dofs=True)
+            self.Ip = fd.get_interpolator(interp)
+            self.Ip.assemble()
             self.CoIp_wstar = fd.Cofunction(self.V_r.dual())
+
             restr = fd.interpolate(fd.TestFunction(self.V_c), self.CoIp_wstar,
-                                   default_missing_val=0.)
-            self.CoIp = fd.Interpolator(restr, self.V_r,
-                                        allow_missing_dofs=True)
+                                   default_missing_val=0., allow_missing_dofs=True)
+            self.CoIp = fd.get_interpolator(restr)
+            self.CoIp.assemble()
 
         elif element.family() == 'Discontinuous Lagrange':
             self.is_DG = True
@@ -192,10 +195,12 @@ class FeControlSpace(ControlSpace):
             # Create interpolator and cointerpolator from V_c into V_r
             self.Ip_v = fd.Function(self.V_c)
             interp = fd.interpolate(self.Ip_v, self.V_r)
-            self.Ip = fd.Interpolator(interp, self.V_r)
+            self.Ip = fd.get_interpolator(interp)
+            self.Ip.assemble()
             self.CoIp_wstar = fd.Cofunction(self.V_r.dual())
             restr = fd.interpolate(fd.TestFunction(self.V_c), self.CoIp_wstar)
-            self.CoIp = fd.Interpolator(restr, self.V_r)
+            self.CoIp = fd.get_interpolator(restr)
+            self.CoIp.assemble()
 
     def restrict(self, residual, out):
         if getattr(self, "is_DG", False):
